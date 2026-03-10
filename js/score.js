ab39388568f68f5bd589112dfa316fc3bd895a9a
MathGeniusGD <mathgenius362@gmail.com>
2026-03-09 23:31:19 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,41 +1,30 @@
-/**
- * Numbers of decimal digits to round to
- */
 const scale = 3;
 
-/**
- * Calculate the score awarded when having a certain percentage on a list level
- * @param {Number} rank Position on the list
- * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
- * @returns {Number}
- */
-export function score(rank, percent, minPercent) {
-    if (rank > 150) {
-        return 0;
+function getRawSum(totalLevels) {
+    let sum = 0;
+    const k = 1.18;
+    const N = totalLevels;
+    for (let i = 1; i <= N; i++) {
+        sum += 3 + 297 * Math.pow((N - i) / Math.max(1, N - 1), k);
     }
-    if (rank > 150 && percent < 100) {
-        return 0;
-    }
+    return sum;
+}
 
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
-    let base = 300 * Math.exp(-0.0383 * (rank - 1));
+export function score(rank, totalLevels, percent, minPercent) {
+    if (percent < minPercent) return 0;
 
-    let score = base *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    const k = 1.18;
+    const N = totalLevels;
 
-    score = Math.max(0, score);
+    let rawScore = 3 + 297 * Math.pow((N - rank) / Math.max(1, N - 1), k);
+    let rawSum = getRawSum(N);
+    let base10kScore = (rawScore / rawSum) * 10000;
 
+    let finalScore = base10kScore * ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     if (percent != 100) {
-        return round(score - score / 3);
+        return round(finalScore - finalScore / 3);
     }
-
-    return Math.max(round(score), 0);
+    return Math.max(round(finalScore), 0);
 }
 
 export function round(num) {
@@ -43,14 +32,7 @@ export function round(num) {
         return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
     } else {
         var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
+        var sig = +arr[1] + scale > 0 ? '+' : '';
+        return +(Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) + 'e-' + scale);
     }
 }
