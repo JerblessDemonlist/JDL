7cea2c18655d5d817ae39004a359b591968fa2ef
Zovori <isthisusernametakenplshelp@gmail.com>
2026-03-17 03:54:19 +1030

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import JerblessList from './pages/JerblessList.js';
+import Packs from './pages/Packs.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/jerbless', component: JerblessList },
+    { path: '/packs', component: Packs },
 ];
