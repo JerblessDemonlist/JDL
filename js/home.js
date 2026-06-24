4a5f223ff2163bcdf0189176154b288873483b10
Zovori <isthisusernametakenplshelp@gmail.com>
2026-06-24 21:30:38 +0930

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,10 @@
-hello world!
+export default {
+    template: `
+        <main class="page-home">
+            <div class="home-content">
+                <h1>Welcome to the Jerbless Demonlist</h1>
+                <p>This page is currently under construction. Check out the List, Leaderboard, or Packs pages in the meantime!</p>
+            </div>
+        </main>
+    `,
+};
